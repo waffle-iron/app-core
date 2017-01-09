29ca92e7252ef0d4d21c5d7ab4bf5267532ae51d
Jose Constela <joseconstela@icloud.com>
2017-01-10 00:47:11 +0100

    Only redirect on success

--- client/modules/myorchards/hooks.js
+++ client/modules/myorchards/hooks.js
@@ -2,10 +2,14 @@
 AutoForm.addHooks(['myOrchardsInsert', 'myOrchardsUpdate'], {
   after: {
     insert: function(error, docId) {
+      if (error) return
+      
       if (docId)
-      Router.go( 'myorchards.one', { _id: docId } );
+        Router.go( 'myorchards.one', { _id: docId } );
     },
     update: function(error, result) {
+      if (error) return
+
       if (this.currentDoc && this.currentDoc._id)
       Router.go( 'myorchards.one', { _id: this.currentDoc._id } );
     }
