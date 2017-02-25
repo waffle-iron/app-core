9cbfcc309cb63d49ed290b0c3b5ebcfa10233a6b
Jose Constela <jose@joseconstela.com>
2017-02-25 17:05:59 +0100

    Move DarkSky api key to env var

--- server/modules/tools/weather/methods.js
+++ server/modules/tools/weather/methods.js
@@ -1,7 +1,7 @@
 Meteor.methods({
   'weatherDetails': (location) => {
     try {
-      let url = `https://api.darksky.net/forecast/bfdcff3a6ef5f21e3bef0f2df3e885ec/${location}?lang=es&units=auto`
+      let url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${location}?lang=es&units=auto`
       return HTTP.call('GET', url).data;
     } catch (e) {
       // Got a network error, time-out or HTTP error in the 400 or 500 range.
