f537c8bd1f6f693f930f72b5f744b42a35faed47
Jose Constela <jose@joseconstela.com>
2017-03-05 19:38:48 +0100

    Remove Google Maps

--- client/modules/myorchards/myorchards.js
+++ client/modules/myorchards/myorchards.js
@@ -19,20 +19,6 @@
  */
 
 Template.myOrchards.onRendered(function() {
-  GoogleMaps.load({ v: '3', key: 'AIzaSyBdHhCYmwljyGMK4qE4Bk0yO2mEKzlv6ko', libraries: 'geometry,places' })
-})
-
-Template.myOrchards.helpers({
-  exampleMapOptions: function() {
-    // Make sure the maps API has loaded
-    if (GoogleMaps.loaded()) {
-      // Map initialization options
-      return {
-        center: new google.maps.LatLng(-37.8136, 144.9631),
-        zoom: 8
-      }
-    }
-  }
 })
 
 Template.myOrchardPlantsTableItem.events({
